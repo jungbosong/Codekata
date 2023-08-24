aabb29d4befe9a46b105b350ad37f062dfc4bea5
bosomi <she79560@naver.com>
2023-08-24 10:21:53 +0900

    [ADD] 짝수와 홀수 (-지연)
    1일차 level1-1번째 문제

---
+++ CodeKata/CodeKata/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeKata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            Console.Write(CheckEven(input));
+
+            // 풀이 2
+            Console.Write(input % 2 == 0 ? "Even" : "Odd");
+        }
+
+        static string CheckEven(int num)
+        {
+            if (num % 2 == 0 || num == 0)
+            {
+                return "Even";
+            }
+            return "Odd";
+        }
+    }
+}
