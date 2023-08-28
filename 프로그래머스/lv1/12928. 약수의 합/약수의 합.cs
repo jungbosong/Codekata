c010fc0486ee116f4940d541f9a0f093b613d0a3
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2023-08-28 09:58:13 +0900

    [level 1] Title: 약수의 합, Time: 0.25 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.cs
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.cs
@@ -1,10 +1,6 @@
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        if (n == 0)
-        {
-            return answer;
-        }
         for(int i = 1; i <= n; i++)
         {
             if(n % i == 0) answer += i;
