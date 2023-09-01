aa4926f7502f0e2836fbcfb6f7cdf5b0948c3732
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2023-09-01 09:37:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.16 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool solution(int x) {
+        bool answer = true;
+        
+        int sum = 0;
+        int tmp = x;
+        while(tmp != 0)
+        {
+            sum += tmp % 10;
+            tmp /= 10;
+        }
+        
+        answer = (x % sum == 0) ? true : false;
+        return answer;
+    }
+}
