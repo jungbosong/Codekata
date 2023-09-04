20020b0c7db620d2cc9f28be71b3ee1ff47c5c10
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2023-09-05 01:09:40 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.20 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
@@ -1,14 +1,8 @@
+using System;
 public class Solution {
     public long solution(int a, int b) {
-        long answer = 0;
-        int start = ( a < b) ? a: b;
-        int end = (start == b) ? a: b;
-        
-        for(int i = start; i <= end; i++)
-        {
-            answer += i;
-        }
-        
-        return answer;
+       long answer = 0;
+
+          return ((long)(Math.Abs(a - b) + 1) * (a + b)) / 2;
     }
 }
