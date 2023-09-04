edc4700edaa2d4f3413ced9511c3a157204b6cf0
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2023-09-04 21:58:49 +0900

    [level 1] Title: 하샤드 수, Time: 2.37 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cs
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cs
@@ -1,16 +1,14 @@
+using System.Linq;
+
 public class Solution {
     public bool solution(int x) {
         bool answer = true;
-        
-        int sum = 0;
-        int tmp = x;
-        while(tmp != 0)
-        {
-            sum += tmp % 10;
-            tmp /= 10;
-        }
-        
-        answer = (x % sum == 0) ? true : false;
+
+        var temp = x.ToString().ToList().Select(y => int.Parse(y.ToString())).Sum();
+
+        if (x % temp != 0)
+            answer = false;
+
         return answer;
     }
 }
