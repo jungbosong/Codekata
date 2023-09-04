fa63628f65636ff0baea18683e1b37613344259c
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2023-09-05 01:29:49 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.50 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -1,13 +1,11 @@
+using System;
+
 public class Solution {
     public string solution(string[] seoul) {
-        string answer = "";
-        for(int i = 0; i < seoul.Length; i++)
-        {
-            if(seoul[i] == "Kim")
-            {
-                answer = $"김서방은 {i}에 있다";
-            }
-        }
-        return answer;
+        int answer = 0;
+
+        answer = Array.FindIndex(seoul, i => i == "Kim");
+
+        return string.Format("김서방은 {0}에 있다",answer);
     }
 }
