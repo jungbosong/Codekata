630277da59d654b95e70fe773e150af0dd494ef4
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2023-09-20 21:10:54 +0900

    [level 1] Title: 최소직사각형, Time: 0.44 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cs
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cs
@@ -3,29 +3,27 @@ using System.Linq;
 
 public class Solution {
     public int solution(int[,] sizes) {
-        int answer = 0;
-        int length = sizes.GetLength(0);
-        int[] widthSizes = new int[length];
-        int[] heightSizes = new int[length];
-        for(int i = 0; i < length; i++)
-        {
-            widthSizes[i] = sizes[i,0];
-            heightSizes[i] = sizes[i,1];
-        }
+        int maxWidth= 0;
+        int maxHeight = 0;
         
-        for(int i = 0;  i< length; i++)
+        for(int i = 0; i < sizes.GetLength(0); i++)
         {
-            if(widthSizes[i] < heightSizes[i])
+            if(sizes[i,0] < sizes[i,1])
             {
-                int tmp = heightSizes[i];
-                heightSizes[i] =  widthSizes[i];
-                widthSizes[i] = tmp;
+                int tmp = sizes[i,1];
+                sizes[i,1] = sizes[i,0];
+                sizes[i,0] = tmp;
+            }
+            if(maxWidth < sizes[i,0])
+            {
+                maxWidth = sizes[i,0];
+            }
+            if(maxHeight < sizes[i,1])
+            {
+                maxHeight = sizes[i,1];
             }
         }
         
-        widthSizes = widthSizes.OrderByDescending(n => n).ToArray();
-        heightSizes = heightSizes.OrderByDescending(n => n).ToArray();
-        answer = widthSizes[0] * heightSizes[0];
-        return answer;
+        return maxWidth * maxHeight;
     }
 }
