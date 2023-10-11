8689dd8d762f04ffe70d4c53a7afecab38047576
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2023-10-11 09:27:12 +0900

    [level 1] Title: 콜라 문제, Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -2,14 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
-        
-        while(n >= a)
-        {
-            answer += n/a * b;
-            n = (n/a)*b + n%a;
-        }
-        
-        return answer;
+        return (n > b ? n - b : 0) / (a - b) * b;
     }
 }
