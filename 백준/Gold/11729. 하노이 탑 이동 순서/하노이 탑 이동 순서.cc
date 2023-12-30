899e4f712b1afcce211341220e66551994f093e2
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2023-12-30 16:00:34 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 124 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -2,20 +2,24 @@
 
 using namespace std;
 
-void hanoi(int n, int start, int to, int bypass)
+void func(int a, int b, int n)
 {
-    if(n == 1)
-        printf("%d %d\n", start, to);
-    else
-    {
-        hanoi(n-1,start,bypass,to);
-        printf("%d %d\n",start,to);
-        hanoi(n-1,bypass,to,start);
+    if(n == 1) {
+        cout << a << ' ' << b << '\n';
+        return;
     }
+    func(a, 6-a-b, n-1);
+    cout << a << ' ' << b << '\n';
+    func(6-a-b, b, n-1);
 }
-int main() {
-    int num;
-    cin >> num;
-    cout << (1<<num) -1 << "\n";
-    hanoi(num,1,3,2);
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int k;
+    cin >> k;
+    cout << (1<<k) -1 << '\n';
+    func(1,3, k);
+    return 0;
 }
