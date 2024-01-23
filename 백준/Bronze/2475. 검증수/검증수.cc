aa080f458a1b8cfd9e77fd85ed63db2dc192f352
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2024-01-23 15:49:14 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        int num;
+        cin >> num;
+        sum += num * num;
+    }
+    cout << sum % 10;
+}
