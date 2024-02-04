eb3e082dcda836604c57667a5172f7d6c9704cc0
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2024-02-04 14:21:51 +0900

    [Silver IV] Title: 주몽, Time: 8 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> numbers;
+	int num;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		numbers.push_back(num);
+	}
+	sort(numbers.begin(), numbers.end());
+
+	int answer = 0;
+	int startIdx = 0, endIdx = n - 1;
+	int sum = numbers[startIdx] + numbers[endIdx];
+	while (startIdx != endIdx)
+	{
+		if (sum > m)
+		{
+			sum = numbers[startIdx] + numbers[--endIdx];
+		}
+		else if (sum < m)
+		{
+			sum = numbers[++startIdx] + numbers[endIdx];
+		}
+		else
+		{
+			sum = numbers[++startIdx] + numbers[endIdx];
+			answer++;
+		}
+	}
+	cout << answer;
+}
