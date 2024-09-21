9c20bda3a376e16153808d16a9e5a31fbb874b69
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2024-09-21 23:30:46 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 176 ms, Memory: 23944 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;      // 도감에 수록된 포켓몬 수
+    int m;      // 문제 개수
+    map<string, int> pokemon_name;      // 키: 포켓몬 이름
+    map<int, string> pokemon_number;    // 키: 포켓몬 번호
+    string name;     // 이름
+    string question; // 질문    
+
+    // 입력
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> name;
+        pokemon_name.insert({name, i});
+        pokemon_number.insert({i, name});
+    }
+
+    // 질문 입력 받고 답 출력
+    for (int i = 0; i < m; i++)
+    {
+        cin >> question;
+
+        // 질문이 숫자일 경우
+        if(question[0] -'0' <= 9)
+        {
+            cout << pokemon_number[stoi(question)] << '\n';
+        }
+        // 질문이 영어일 경우
+        else
+        {
+             cout << pokemon_name[question] << '\n';
+        }
+    }
+
+    return 0;
+}
