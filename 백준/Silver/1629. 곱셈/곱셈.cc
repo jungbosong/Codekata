cdb5eec37bbd23ab1741a17a4efb905ba350985e
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2024-09-23 21:54:30 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,26 +1,30 @@
-// http://boj.kr/a9f89b45ac624c2a8d13f27a01dd78d1
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
+typedef long long ll;
 
-// int overflow 방지
-using ll = long long;
+ll a, b, c;
 
-// a^b mod m 을 계산하는 함수
-ll POW(ll a, ll b, ll m){
-  // base condition
-  if(b==1) return a % m;
-  ll val = POW(a, b/2, m);
-  val = val * val % m;
-  // b가 짝수일 때, k승을 m으로 나눈 나머지이므로 그대로 val 반환
-  if(b%2 == 0) return val;
-  // b가 홀수일 때, a를 한 번 더 곱해서 k+1승을 m으로 나눈 나머지 반환
-  return val * a % m;
+ll POW(ll a, ll b)
+{
+    if(b == 1)
+    {
+        return a % c;
+    }
+    ll ret = POW(a, b / 2);
+    ret = (ret * ret) % c;
+    if(b % 2) 
+    {
+        ret = (ret * a) % c;
+    }
+    return ret;
 }
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  ll a,b,c;
-  cin >> a >> b >> c;
-  cout << POW(a,b,c);
+int main()
+{
+    cin >> a >> b >> c;
+
+    cout << POW(a, b);
+
+    return 0;
 }
