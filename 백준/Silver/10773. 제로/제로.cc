0d43ee39e0d59b450e0659c45d916c3050497bf5
BOSOMI <52051122+jungbosong@users.noreply.github.com>
2024-10-30 12:10:42 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int k, num;
+    long sum = 0;
+    stack<int> called_num;
+    called_num.push(0);
+
+    cin >> k;
+
+    for(int i = 0; i < k; i++) {
+        cin >> num;
+        if(num == 0) {
+            called_num.pop();
+        }
+        else {
+            called_num.push(num);
+        }
+    }
+
+    while(!called_num.empty())
+    {
+        sum += called_num.top();
+        called_num.pop();
+    } 
+
+    cout << sum;
+
+    return 0;
+}
